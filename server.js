f066a114083f2aab9fbeceacdd37a42dbb812d74
fsevindik <fsevindik2923@gmail.com>
2025-05-04 19:21:31 +0300

    go on bro

--- server.js
+++ server.js
@@ -2,22 +2,35 @@
 // 1. Create a simple HTTP server using Node.js
 
 import http from 'http';
+import fs from 'fs';
+import e from 'express';
 
 
 const server = http.createServer((req, res) => {
-    switch (req.url) {
-        case "/":
-            res.end("home page")
-            break;
-    
-        case "/about":
-            res.end("about page")
-            break;
 
-        default:
-            res.end("404- page not found")
-          
+
+    // guys imagine how computers can save ur all acitons ...):
+fs.appendFile("log.txt", req.url, (err, html)=> {
+    if(err) {
+        throw new Error(err);
+    } else {
+        switch (req.url) {
+            case "/":
+                res.end("home page")
+                break;
+        
+            case "/about":
+                res.end("about page")
+                break;
+    
+            default:
+                res.end("404- page not found")
+              
+        }
     }
+})
+
+   
    
 
 });
